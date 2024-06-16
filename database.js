515c31f9b354d2d72018d998ff82c51b107135e1
Braymsn <kevin.braemer@iu-study.org>
2024-06-16 13:09:27 +0200

    Version 0.11
    - Filter Indoor/Outdoor hinzugefügt
    - Unit Test activityGenerator angepasst

--- database.js
+++ database.js
@@ -1,128 +1,164 @@
 // sqlite3 Modul impoortieren und verbose-Modus aktivieren, der detailliertere Logs liefert
-import sqlite3 from 'sqlite3';
+import sqlite3 from "sqlite3";
 
 // Name der Datenbankdatei definieren
-const dbName = 'activities.db';
+const dbName = "activities.db";
 
 // Erstellen der SQLite-Datenbankdatei
 const db = new sqlite3.Database(dbName, (err) => {
-    if (err) {
-        // Gibt eine Fehlermeldung aus, falls beim Verbinden zur Datenbank ein Fehler auftritt
-        return console.error(err.message);
-    }
-    // Verbindung zur Datenbank wird bestätigt
-    console.log(`Connected to the ${dbName} database.`);
+  if (err) {
+    // Gibt eine Fehlermeldung aus, falls beim Verbinden zur Datenbank ein Fehler auftritt
+    return console.error(err.message);
+  }
+  // Verbindung zur Datenbank wird bestätigt
+  console.log(`Connected to the ${dbName} database.`);
 });
 
 // Funktion für die zufällige Auswahl einer Kategorie
-function getRandomCategory(){
-    const categories = ['Sport', 'Culture', 'Education', 'Information', 'Culinary', 'Housework']
-    return categories[Math.floor(Math.random() * categories.length)];
+function getRandomCategory() {
+  const categories = [
+    "Sport",
+    "Culture",
+    "Education",
+    "Information",
+    "Culinary",
+    "Housework",
+  ];
+  return categories[Math.floor(Math.random() * categories.length)];
+}
+
+function getRandomLocation() {
+  const locations = ["indoor", "outdoor"];
+  return locations[Math.floor(Math.random() * locations.length)];
 }
 
 // Startet die Ausführung der Datenbankoperationen
 db.serialize(() => {
-     // Löscht die bestehende Tabelle 'activities', wenn sie existiert
-    db.run("DROP TABLE IF EXISTS activities", (err) => {
-        if (err) {
-            // Gibt eine Fehlermeldung aus, falls beim Löschen der Tabelle ein Fehler auftritt
-            console.error("Error dropping table:", err.message);
-            return;
-        }
-        // Bestätigt, dass die Tabelle erfolgreich gelöscht wurde
-        console.log('Table dropped');
+  // Löscht die bestehende Tabelle 'activities', wenn sie existiert
+  db.run("DROP TABLE IF EXISTS activities", (err) => {
+    if (err) {
+      // Gibt eine Fehlermeldung aus, falls beim Löschen der Tabelle ein Fehler auftritt
+      console.error("Error dropping table:", err.message);
+      return;
+    }
+    // Bestätigt, dass die Tabelle erfolgreich gelöscht wurde
+    console.log("Table dropped");
 
-        // Tabelle 'activities' erstellen
-        db.run(`CREATE TABLE activities (
+    // Tabelle 'activities' erstellen
+    db.run(
+      `CREATE TABLE activities (
             id INTEGER PRIMARY KEY AUTOINCREMENT, 
             description TEXT NOT NULL,
             participants INTEGER NOT NULL,
-            category TEXT NOT NULL
-        )`, (err) => {
-            if (err) {
-                // Gibt einen Fehler aus, falls beim Erstellen der Tabelle ein Problem aufgetreten ist
-                console.error("Error creating table:", err.message);
-                return;
+            category TEXT NOT NULL,
+            location TEXT NOT NULL
+        )`,
+      (err) => {
+        if (err) {
+          // Gibt einen Fehler aus, falls beim Erstellen der Tabelle ein Problem aufgetreten ist
+          console.error("Error creating table:", err.message);
+          return;
+        }
+        // Bestätigen, dass die Tabelle erfolgreich erstellt wurde
+        console.log("Table created");
+
+        // Liste der Aktivitäten mit Teilnehmeranzahl, die in die Datenbank eingefügt werden sollen
+        const activities = [
+          ...Array.from({ length: 4 }, (_, i) => ({
+            description: "Tennis spielen",
+            participants: i + 1,
+            category: "Sport",
+            location: "outdoor",
+          })),
+          ...Array.from({ length: 2 }, (_, i) => ({
+            description: "Ein Spaziergang im Park",
+            participants: i + 1,
+            category: "Culture",
+            location: "outdoor",
+          })),
+          ...Array.from({ length: 1 }, (_, i) => ({
+            description: "Ein Buch lesen",
+            participants: i + 1,
+            category: "Education",
+            location: "indoor",
+          })),
+          ...Array.from({ length: 2 }, (_, i) => ({
+            description: "Ein neues Rezept kochen",
+            participants: i + 1,
+            category: "Culinary",
+            location: "indoor",
+          })),
+          ...Array.from({ length: 2 }, (_, i) => ({
+            description: "Einen Dokumentarfilm ansehen",
+            participants: i + 1,
+            category: "Information",
+            location: "indoor",
+          })),
+          ...Array.from({ length: 8 }, (_, i) => ({
+            description: "Ein Brettspiel spielen",
+            participants: i + 2,
+            category: "Sport",
+            location: "indoor",
+          })),
+          ...Array.from({ length: 2 }, (_, i) => ({
+            description: "Gartenarbeit machen",
+            participants: i + 1,
+            category: "Housework",
+            location: "indoor",
+          })),
+          ...Array.from({ length: 12 }, (_, i) => ({
+            description: "Volleyball spielen",
+            participants: i + 2,
+            category: "Sport",
+            location: "outdoor",
+          })),
+          ...Array.from({ length: 20 }, (_, i) => ({
+            description: "Ein Museum besuchen",
+            participants: i + 1,
+            category: "Culture",
+            location: "indoor",
+          })),
+          ...Array.from({ length: 5 }, (_, i) => ({
+            description: "Eine Fahrradtour veranstalten",
+            participants: i + 1,
+            category: "Sport",
+            location: "outdoor",
+          })),
+          ...Array.from({ length: 1 }, (_, i) => ({
+            description: "Schreibe eine Kurzgeschichte",
+            participants: i + 1,
+            category: "Education",
+            location: "indoor",
+          })),
+        ];
+
+        for (const activity of activities) {
+          //Falls keine Kategorie angegeben ist, wähle eine zufällige Kategorie
+          if (!activity.category) {
+            activity.category = getRandomCategory();
+          }
+        }
+
+        // Füge jede Aktivität mit Teilnehmeranzahl in die Datenbanktabelle ein
+        activities.forEach((activity) => {
+          db.run(
+            `INSERT INTO activities (description, participants, category, location) VALUES (?, ?, ?, ?)`,
+            [
+              activity.description,
+              activity.participants,
+              activity.category,
+              activity.location,
+            ],
+            (err) => {
+              if (err) {
+                // Gibt eine Fehlermeldung aus, falls beim Einfügen der Daten ein Fehler auftritt
+                console.error("Error inserting data:", err.message);
+              }
             }
-            // Bestätigen, dass die Tabelle erfolgreich erstellt wurde
-            console.log('Table created');
-
-            // Liste der Aktivitäten mit Teilnehmeranzahl, die in die Datenbank eingefügt werden sollen
-            const activities = [
-                ...Array.from({length: 4}, (_, i) => ({
-                    description: 'Tennis spielen',
-                    participants: i + 1,
-                    category: 'Sport'
-                })),
-                ...Array.from({length: 2}, (_, i) => ({
-                    description: 'Ein Spaziergang im Park',
-                    participants: i + 1,
-                    category: 'Culture'
-                })),
-                ...Array.from({length: 1}, (_, i) => ({
-                    description: 'Ein Buch lesen',
-                    participants: i + 1,
-                    category: 'Education'
-                })),
-                ...Array.from({length: 2}, (_, i) => ({
-                    description: 'Ein neues Rezept kochen',
-                    participants: i + 1,
-                    category: 'Culinary'
-                })),
-                ...Array.from({length: 2}, (_, i) => ({
-                    description: 'Einen Dokumentarfilm ansehen',
-                    participants: i + 1,
-                    category: 'Information'
-                })),
-                ...Array.from({length: 8}, (_, i) => ({
-                    description: 'Ein Brettspiel spielen',
-                    participants: i + 2,
-                    category: 'Sport'
-                })),
-                ...Array.from({length: 2}, (_, i) => ({
-                    description: 'Gartenarbeit machen',
-                    participants: i + 1,
-                    category: 'Housework'
-                })),
-                ...Array.from({length: 12}, (_, i) => ({
-                    description: 'Volleyball spielen',
-                    participants: i + 2,
-                    category: 'Sport'
-                })),
-                ...Array.from({length: 20}, (_, i) => ({
-                    description: 'Ein Museum besuchen',
-                    participants: i + 1,
-                    category: 'Culture'
-                })),
-                ...Array.from({length: 5}, (_, i) => ({
-                    description: 'Eine Fahrradtour veranstalten',
-                    participants: i + 1,
-                    category: 'Sport'
-                })),
-                ...Array.from({length: 1}, (_, i) => ({
-                    description: 'Schreibe eine Kurzgeschichte',
-                    participants: i + 1,
-                    category: 'Education'
-                })),
-            ];
-
-            for (const activity of activities) {
-                //Falls keine Kategorie angegeben ist, wähle eine zufällige Kategorie 
-                if (!activity.category) {
-                    activity.category = getRandomCategory();
-                }
-            }
-
-            // Füge jede Aktivität mit Teilnehmeranzahl in die Datenbanktabelle ein
-            activities.forEach(activity => {
-                db.run(`INSERT INTO activities (description, participants, category) VALUES (?, ?, ?)`, [activity.description, activity.participants, activity.category], err => {
-                    if (err) {
-                        // Gibt eine Fehlermeldung aus, falls beim Einfügen der Daten ein Fehler auftritt
-                        console.error("Error inserting data:", err.message);
-                    }
-                });
-            });
-        })
-    });
+          );
+        });
+      }
+    );
+  });
 });
 export default db;
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 // Einbinden der benötigten Module
-import express from 'express';
-import fs from 'fs';
-import sqlite3 from 'sqlite3';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import express from "express";
+import fs from "fs";
+import sqlite3 from "sqlite3";
+import path from "path";
+import { fileURLToPath } from "url";
 
 // Erstellung einer neuen Express-App
 const app = express();
@@ -12,87 +12,104 @@ const app = express();
 const port = 3000;
 
 // Verbindung zur SQLite-Datenbank
-const db = new sqlite3.Database('./activities.db', (err) => {
+const db = new sqlite3.Database("./activities.db", (err) => {
   if (err) {
     // Gibt eine Fehlermeldung aus, wenn die Verbindung zur Datenbank fehlschlägt
-      console.error(err.message);
+    console.error(err.message);
   }
   // Bestätige die erfolgreiche Verbindung zur Datenbank
-  console.log('Connected to the activities database.');
+  console.log("Connected to the activities database.");
 });
 
 // Einfaches Cache-Objekt als Ersatz für HttpContext.Cache --> Ersatz für eine komplexe Cashing-Strategie
 const cache = {};
 
 // Konfiguration des Servers, um statische Dateien aus dem "public"-Verzeichnis zu bedienen
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-// Funktion zur Suche nach einer Aktivität basierend auf der Teilnehmeranzahl und Kategorie
-function fetchActivityByCategory(participants, category, res, callback) {
-  db.get("SELECT description FROM activities WHERE participants = ? AND category = ? ORDER BY RANDOM() LIMIT 1", [participants, category], (err, row) => {
+// Funktion zur Suche nach einer Aktivität basierend auf der Teilnehmeranzahl, Kategorie und Standort
+function fetchActivityByCategory(
+  participants,
+  category,
+  location,
+  res,
+  callback
+) {
+  db.get(
+    "SELECT description FROM activities WHERE participants = ? AND category = ? AND location = ? ORDER BY RANDOM() LIMIT 1",
+    [participants, category, location],
+    (err, row) => {
       if (err) {
-          console.error("Database error: ", err.message);
-          return res.status(500).send("Error fetching activity");
-      } 
+        console.error("Database error: ", err.message);
+        return res.status(500).send("Error fetching activity");
+      }
 
       if (row) {
         res.json({ category: category, description: row.description });
       } else {
-          callback();
+        callback();
       }
-  });
+    }
+  );
 }
 
-app.get('/random-activity', (req, res) => {
+app.get("/random-activity", (req, res) => {
   // Teilnehmeranzahl wird aus der Anfrage entnommen. Wenn keine Teilnhemeranzahl angegeben ist -> 1
-  const participants = parseInt(req.query.participants) || 1; 
+  const participants = parseInt(req.query.participants) || 1;
   // Kategorie wird aus der Anfrage enttnommen
   let category = req.query.category;
+  const location = req.query.location;
 
-  if (category === 'random') {
-      // Abrufen aller Kategorien
-      db.all("SELECT DISTINCT category FROM activities", (err, rows) => {
-          if (err) {
-            // Bei einem Datenbankfehler wird eine Fehlermeldung ausgegeben 
-              console.error("Database error: ", err.message);
-              return res.status(500).send("Error fetching categories");
-          }
+  if (category === "random") {
+    // Abrufen aller Kategorien
+    db.all("SELECT DISTINCT category FROM activities", (err, rows) => {
+      if (err) {
+        // Bei einem Datenbankfehler wird eine Fehlermeldung ausgegeben
+        console.error("Database error: ", err.message);
+        return res.status(500).send("Error fetching categories");
+      }
 
-          // Wenn keine Kategorien gefunden werden
-          if (rows.length === 0) {
-              return res.status(404).send("No categories found");
-          }
+      // Wenn keine Kategorien gefunden werden
+      if (rows.length === 0) {
+        return res.status(404).send("No categories found");
+      }
 
-          // Kategorien mischen und durchgehen
-          const shuffledCategories = rows.sort(() => 0.5 - Math.random());
-          let index = 0;
+      // Kategorien mischen und durchgehen
+      const shuffledCategories = rows.sort(() => 0.5 - Math.random());
+      let index = 0;
 
-          // Funktion, um die nächste Kategorie zu versuchen
-          const tryNextCategory = () => {
-              if (index < shuffledCategories.length) {
-                // setzt die Kategorie auf die nächste in der gemischten Liste
-                  category = shuffledCategories[index].category;
-                  index++;
-                  //Versucht, eine Aktivität in der aktuellen Kategorie zu finden
-                  fetchActivityByCategory(participants, category, res, tryNextCategory);
-              } else {
-                // Wenn keine Aktivitäten gefunden werden, wird eine Fehlermeldung ausgegeben
-                  res.status(404).send("Keine Aktivitäten gefunden");
-              }
-          };
-
-          tryNextCategory(); // Suche starten
-      });
-  } else {
-      // Aktivität in der angegebenen Kategorie suchen
-      fetchActivityByCategory(participants, category, res, () => {
+      // Funktion, um die nächste Kategorie zu versuchen
+      const tryNextCategory = () => {
+        if (index < shuffledCategories.length) {
+          // setzt die Kategorie auf die nächste in der gemischten Liste
+          category = shuffledCategories[index].category;
+          index++;
+          //Versucht, eine Aktivität in der aktuellen Kategorie zu finden
+          fetchActivityByCategory(
+            participants,
+            category,
+            location,
+            res,
+            tryNextCategory
+          );
+        } else {
+          // Wenn keine Aktivitäten gefunden werden, wird eine Fehlermeldung ausgegeben
           res.status(404).send("Keine Aktivitäten gefunden");
-      });
+        }
+      };
+
+      tryNextCategory(); // Suche starten
+    });
+  } else {
+    // Aktivität in der angegebenen Kategorie suchen
+    fetchActivityByCategory(participants, category, location, res, () => {
+      res.status(404).send("Keine Aktivitäten gefunden");
+    });
   }
 });
 
 // Inhalte werden mit Versionsinformation versehen durch GET
-app.get('/versioned-content', (req, res) => {
+app.get("/versioned-content", (req, res) => {
   try {
     const contentPath = req.query.path; // Pfad der angeforderten Datei
     const versionedPath = versionedContent(contentPath); // Versionierter Pfad wird erzeugt
@@ -110,23 +127,30 @@ function versionedContent(contentPath) {
     const __filename = fileURLToPath(import.meta.url);
     const __dirname = path.dirname(__filename);
 
-    const physicalPath = path.join(__dirname, 'public', contentPath); // Vollständiger Pfad der Datei
-    console.log('Trying to access:', physicalPath);
+    const physicalPath = path.join(__dirname, "public", contentPath); // Vollständiger Pfad der Datei
+    console.log("Trying to access:", physicalPath);
     const fileInfo = fs.statSync(physicalPath); // Hole Dateiinformationen
-    const version = "v=" + fileInfo.mtime.toISOString().replace(/[^0-9]/g, "").substring(0, 14); // Versionsstring wird erzeugt
+    const version =
+      "v=" +
+      fileInfo.mtime
+        .toISOString()
+        .replace(/[^0-9]/g, "")
+        .substring(0, 14); // Versionsstring wird erzeugt
 
     const translatedContentPath = contentPath;
     const versionedContentPath = contentPath.includes("?")
       ? `${translatedContentPath}&${version}` // Version wird zu einem Pfad mit vorhandenen Query-Parametern hinzugefügt
       : `${translatedContentPath}?${version}`; // Füge Version zu einem Pfad ohne Query-Parameter hinzu
 
-     // Speichere den versionierten Pfad im Cache und stelle ihn nach 1 Minute zum Löschen ein
+    // Speichere den versionierten Pfad im Cache und stelle ihn nach 1 Minute zum Löschen ein
     cache[contentPath] = versionedContentPath;
-    setTimeout(() => { delete cache[contentPath]; }, 60000); // Cache für 1 Minute
+    setTimeout(() => {
+      delete cache[contentPath];
+    }, 60000); // Cache für 1 Minute
 
     return versionedContentPath;
   } else {
-    return cache[contentPath];  // Liefert den Pfad aus dem Cache, falls vorhanden
+    return cache[contentPath]; // Liefert den Pfad aus dem Cache, falls vorhanden
   }
 }
 
